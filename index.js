26938c4dbaa48b89f82d4d0f3af0be2a9d82dc35
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-11 21:39:29 +0900

    computedプロパティで動的な変数を扱う

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message:null
+        number:0
+    },
+    
+    computed: {
+        Text1: function(){
+            return this.number > 3 ? '３よりうえ' : '３以下'
+        }
     }
 })
