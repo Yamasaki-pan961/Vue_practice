6fa26170dfe413bd0c137c79ed39cf7c7d88fa0f
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-11 15:13:01 +0900

    v-bindのいろんな機能を使った。

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var app = new Vue({
     data: {
         message: 'Hello World!',
         url1: "https://www.google.co.jp/",
-        url2: "https://www.yahoo.co.jp/",
-        title:'<h1>うんこ<h1>'
-        
+        attribute:"href",
+        YahooObject:{
+            href:"https://www.yahoo.co.jp/",
+            id:69
+        }   
     },
+
     methods:{
         Hello: function(){
             this.message='こんちは'
