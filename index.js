7c10519849748882fc954bed6c6744e7288b11d8
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-11 21:21:26 +0900

    v-modelで双方向干渉をおこなう

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        x:0,
-        y:0
-    },
-
-    methods:{
-        MousePoint: function(event){
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        myAlert(){
-            alert('スペースおした');
-        } 
+        message:null
     }
 })
