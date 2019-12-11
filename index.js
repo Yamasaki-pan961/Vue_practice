ec6b646b49a9c4aad5ca39a7b8bc4c63514a18b1
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-11 15:49:04 +0900

    v-onのイベント情報を取得して、マウスの座標を取得

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        number:0,
-        n:0
+        x:0,
+        y:0
     },
 
     methods:{
-        CountUp: function(){
-            this.n++;
+        MousePoint: function(event){
+            this.x = event.clientX;
+            this.y = event.clientY;
+
         }
     }
 })
