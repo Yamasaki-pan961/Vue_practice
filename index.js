33ad434d2004269739ae40656b495c8545f05581
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-11 21:56:28 +0900

    {{method}}は描画毎に実行されるから、{{computed}}を使いましょう

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        number:0
+        number:0,
+        n:0
     },
     
     computed: {
-        Text1: function(){
+        TextComputed: function(){
+            console.log("computed");
+            return this.number > 3 ? '３よりうえ' : '３以下'
+        }
+    },
+    methods:{
+        TextMethod : function(){
+            console.log("method")
             return this.number > 3 ? '３よりうえ' : '３以下'
         }
     }
