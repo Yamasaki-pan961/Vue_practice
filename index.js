6c2456f245b42ee055418b27e10b78b410edbadd
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-22 00:38:04 +0900

    classを動的に指定する

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 var app = new Vue({
-    el: '#app',
+    el: '#app', 
     data: {
-        number:0,
-        n:0
-    },
-    
-    computed: {
-        TextComputed: function(){
-            console.log("computed");
-            return this.number > 3 ? '３よりうえ' : '３以下'
-        }
-    },
-    watch:{
-        number: function(){
-            var that=this;//なんかthisは使えんから別の奴にする
-            if(that.number == 1){
-                setTimeout(function(){
-                    that.number = 0;
-                },3000)
-            }
-        }
+        isActive: true
     }
 })
