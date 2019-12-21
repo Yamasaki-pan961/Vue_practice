fdd4b014aafdadb6dece886e0bafcc3bf475b866
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-22 01:17:00 +0900

    テンプレート構文まとめました

---
+++ テンプレート構文/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app', 
+    data: {
+        styleObject:{
+            color:'red'
+            ,'background-color':'blue'
+        },
+        baseObject:{
+            'fontSize':'60px'
+        }
+    }
+})
