81be774fc4d74d5903b8c3541b3c7cbfe59b491b
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-22 10:13:09 +0900

    v-forでオブジェクトを表示する

--- リストレンダリング/index.js
+++ リストレンダリング/index.js
@@ -2,5 +2,11 @@ new Vue({
     el:'#app'
     ,data:{
         agree:　['こんにちは','Hello','カムサハムニダ']
+        ,object:{
+            country: 'Japan'
+            ,age: 19
+            ,firstname:'Ataushi'
+            ,famliyname:'Yamasaki'
+        }
     }
 })
