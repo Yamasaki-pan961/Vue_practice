49a1944a26b702352ac50d38766dcc70b3d270d0
Yamasaki-pan961 <atusi08250522@gmail.com>
2019-12-23 09:34:27 +0900

    インスタンス内から$dataにアクセスする

--- Vueインスタンス/index.js
+++ Vueインスタンス/index.js
@@ -6,6 +6,11 @@ var data = {
 var vm = new Vue({
     el:'#app1'
     ,data:data
+    ,computed:{
+        Mydata:function(){
+            return this.$data;
+        }
+    }
 })
 
 console.log(data===vm.$data);
