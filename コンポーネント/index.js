252b0a5fa244bfb7b534dd9696f17cd22ab0cbdc
unknown <atusi@aaa.center.tsuyama-ct.ac.jp>
2019-12-28 23:22:33 +0900

    ローカルコンポーネントを一部のインスタンスに適応

--- コンポーネント/index.js
+++ コンポーネント/index.js
@@ -1,14 +1,17 @@
-Vue.component('Like',{
+var component = {
     data:　function(){
         return {
             number:1
         }
     },
     template:'<div><p>いいね{{number}}個</p><button v-on:click="number++">+1</button><hr></div>'
-})
+}
 
 new Vue({
-    el:'#app'
+    el:'#app',
+    components:{
+        Like:component
+    }
 })
 
 new Vue({
