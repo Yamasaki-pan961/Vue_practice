0e221c8a094d3e77d42b806124a8e92370a2be81
unknown <atusi@aaa.center.tsuyama-ct.ac.jp>
2019-12-28 18:33:51 +0900

    VueCLIをビルドした

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue'//シングルファイルコンポーネントのvueファイル
 
+//開発モードの選択
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App),//ES6の書き方
+  //コンポーネントのオブジェクトApp.vueを引数としている
 }).$mount('#app')
