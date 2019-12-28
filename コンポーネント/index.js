0409b3823948cca2eb98feb387741e1742dea5f9
unknown <atusi@aaa.center.tsuyama-ct.ac.jp>
2019-12-28 22:31:29 +0900

    コンポーネント別にデータを保持している

--- コンポーネント/index.js
+++ コンポーネント/index.js
@@ -4,9 +4,9 @@ Vue.component('Like',{
             number:12
         }
     },
-    template:'<p>いいね{{number}}</p>'
+    template:'<div><p>いいね{{number}}個</p><button v-on:click="number++">+1</button><hr></div>'
 })
+
 new Vue({
     el:'#app'
-
 })
